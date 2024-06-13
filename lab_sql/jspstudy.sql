48e7a5c3557c4436491809603b836f2f0c2a4abf
gusdn99 <167641195+gusdn99@users.noreply.github.com>
2024-06-13 19:39:34 +0900

    24-06-13

--- lab_sql/jspstudy.sql
+++ lab_sql/jspstudy.sql
@@ -37,6 +37,36 @@ commit;
 
 select * from users order by id desc;
 
+create table comments (
+    id              number(6) generated as identity,
+    post_id         number(6),
+    username        varchar2(20) not null,
+    ctext           varchar2(1000 char) not null,
+    created_time    timestamp default systimestamp,
+    modified_time   timestamp default systimestamp,
+    constraint comments_id_pk primary key (id),
+    constraint comments_post_id_fk foreign key (post_id) references posts (id)
+);
 
+select * from comments order by id desc;
+
+insert into comments (post_id, username, ctext)
+values (46, 'test', 'test comment');
+
+commit;
+
+select * from comments where post_id = 28 order by id desc;
+
+update comments
+set ctext = '댓글 업데이트', modified_time = systimestamp
+where id = 1;
+
+commit;
+
+delete from comments where id = 2;
+
+commit;
+
+select count(*) from comments where post_id = 2;
 
 
