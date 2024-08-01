a37a02a7d975d5999c24acbf7686a6d0cdf95291
gusdn99 <167641195+gusdn99@users.noreply.github.com>
2024-08-01 17:40:30 +0900

    24-08-01

--- lab_sql/bootstudy.sql
+++ lab_sql/bootstudy.sql
@@ -15,3 +15,21 @@ insert into posts (title, content, author)
 values ('배고파진다', '배고픈데 먹을 게 읍따', 'user');
 
 commit;
+
+create table comments (
+    id                          number(9) generated as identity,
+    post_id                     number(9) not null,
+    ctext                       varchar2(500 char) not null,
+    writer                      varchar2(20 char) not null,
+    created_time                timestamp,
+    modified_time               timestamp,
+    constraint comments_pk_id   primary key (id)
+);
+
+alter table comments
+add constraint fk_comments_ref_posts
+    foreign key (post_id) references posts (id)
+    on delete cascade;
+
+-- delete cascade: posts 테이블의 id가 삭제되면 comments 테이블의 모든 post_id가 삭제됨.
+    
